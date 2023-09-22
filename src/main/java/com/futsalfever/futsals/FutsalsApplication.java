ec85eded681f8a54fdcf8c4e6a4c50590aa580d7
Prajjwall123 <121224338+Prajjwall123@users.noreply.github.com>
2023-09-22 15:02:51 +0545

    First Commit

---
+++ src/main/java/com/futsalfever/futsals/FutsalsApplication.java
@@ -0,0 +1,13 @@
+package com.futsalfever.futsals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutsalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutsalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futsalfever/futsals/FutsalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futsalfever.futsals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutsalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
